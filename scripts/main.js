53c4fcc145a83c54d6e0398619f12f141c8a3d72
seralv <sergioalvaro.14@gmail.com>
2024-12-09 05:51:43 -0400

    Beginning form UI format

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 const myLibrary = ["El principito", "Mallku", "Raza de Bronce"];
+const modalButton = document.getElementById("updateDetails");
+modalButton.addEventListener("click", function () {
+  modalAddBook();
+});
 
-function Book() {}
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
 function addBookToLibrary() {}
 
@@ -8,6 +17,22 @@ function readMyLibrary() {
   for (const book in myLibrary) {
     console.log(`${parseInt(book) + 1}: ${myLibrary[book]}`);
   }
+}
+
+function modalAddBook() {
+  var updateButton = document.getElementById("updateDetails");
+  var cancelButton = document.getElementById("cancel");
+  var favDialog = document.getElementById("favDialog");
+
+  // Update button opens a modal dialog
+  updateButton.addEventListener("click", function () {
+    favDialog.showModal();
+  });
+
+  // Form cancel button closes the dialog box
+  cancelButton.addEventListener("click", function () {
+    favDialog.close();
+  });
 }
 
 readMyLibrary();
